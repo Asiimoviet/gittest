b0be00cba3a40e284c0501c98778bf3d788d6f11
Asiimoviet <asiimoviet@gmail.com>
2022-11-27 21:13:24 -0800

    My change

--- test.cpp
+++ test.cpp
@@ -8,6 +8,8 @@ int main()
     cout << "From Online" << endl;
     
     cout << "This is another another one" << endl;
+
+    cout << "Fuck you google" << endl;
     
     cout << "This is another another another one" << endl;
     
